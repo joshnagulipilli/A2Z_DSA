2f47c82733c044e88dcbca4c8876207711c4241b
him <joshnagulipilli@gmail.com>
2024-09-30 15:57:03 +0530

    trial

--- Prime/TrialDivMethod.cpp
+++ Prime/TrialDivMethod.cpp
@@ -8,7 +8,7 @@ bool isPrime(int n)
     if(n%2==0 || n%3==0) return false;
     for(int i=5;i*i<=n;i+=6)
     {
-        if( n % i == 0 || n % (i+2) == 0)
+        if( n % i == 0 || n % (i+2) == 0) //here we check for 11 and 13 . every number have 6 diff and 2 diff
         {
             return false;
         }
