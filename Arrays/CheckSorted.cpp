6c1f1ea424133abd62393225da72ad73489add5f
him <joshnagulipilli@gmail.com>
2024-09-30 13:12:07 +0530

    message

--- Arrays/CheckSorted.cpp
+++ Arrays/CheckSorted.cpp
@@ -1,17 +1,39 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int brute(vector<int>& arr)
+bool brute(vector<int>& arr)   //o(n*n) + o(1)
 {
-
+    bool flag=0;
+    for(int i=0;i<arr.size()-1;i++)
+    {
+        for(int j=i+1;j<arr.size();j++)
+        {
+            if(arr[i]>arr[j])
+            {
+                return false;
+            }
+        }
+    }
+    return true;
+    
 }
 int better(vector<int>& arr)
 {
 
 }
-int optimal(vector<int>& arr)
+bool optimal(vector<int>& arr)      // o(n-1) + o(1)
 {
+     bool flag=0;
     
+        for(int j=1;j<arr.size();j++)
+        {
+            if(arr[j-1]>arr[j])
+            {
+                return false;
+            }
+        
+    }
+    return true;
 }
 
 
@@ -26,6 +48,6 @@ int main()
         cin>>arr[i];
     }
     cout<<brute(arr)<<endl;
-    cout<<better(arr)<<endl;
+    // cout<<better(arr)<<endl;
     cout<<optimal(arr)<<endl;
 }
