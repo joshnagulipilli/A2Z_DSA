d27979af738e08ada55e3560f449dbfb072b6fe1
him <joshnagulipilli@gmail.com>
2024-10-08 11:48:16 +0530

    move

--- Arrays/Hard/ThreeSum.cpp
+++ Arrays/Hard/ThreeSum.cpp
@@ -48,17 +48,17 @@ vector<vector<int>> better(vector<int>& arr,int sum)
 }
 
 
-vector<vector<int>> optimal(vector<int>& arr,int sum)  
+vector<vector<int>> optimal(vector<int>& arr,int sum)    //o(nlogn + n*n)  + o(no of triplets - x say)
 {
-           vector<vector<int>>ans;
-           sort(arr.begin(),arr.end());
-           int n=arr.size();
+           vector<vector<int>>ans;                           //space o(no .of triplets)
+           sort(arr.begin(),arr.end());                      //o(nlogn)
+           int n=arr.size();                                 //o(n)
            for(int i=0;i<n;i++)
            {
             if(i>0 && arr[i]==arr[i-1]) continue;
             int j=i+1;
             int k=n-1;
-            while(j<k)
+            while(j<k)                                    //o(n-1,n-2,n-3..........n-n)
             {
                 int res=arr[i]+arr[k]+arr[j];
                 if(res==sum)
@@ -66,7 +66,7 @@ vector<vector<int>> optimal(vector<int>& arr,int sum)
                     ans.push_back({arr[i],arr[j],arr[k]});
                     j++;
                     k--;
-                    while(j<k && arr[j]==arr[j-1]) j++;
+                    while(j<k && arr[j]==arr[j-1]) j++;               // o(min time )
                     while(j<k && arr[k]==arr[k+1]) k--;
                 }
                 else if(sum>0) k--;
